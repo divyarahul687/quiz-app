62ca2dd2d89445ee01657620df38afb15962bf03
rahulnpv <npvrahul7@gmail.com>
2025-07-20 15:38:40 +0530

    add chesamu

---
+++ script.js
@@ -0,0 +1,7 @@
+<button id="toggleButton">Toggle Content</button>
+<div id="content">This is some content to show/hide.</div>
+<form id="myForm">
+  <input type="text" id="name" placeholder="Enter your name" required />
+  <button type="submit">Submit</button>
+</form>
+<script src="script.js"></script>
